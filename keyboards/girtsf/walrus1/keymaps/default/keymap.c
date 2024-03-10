a7f896154e0b6d93e8ac48f02b144b939aab27b1
Girts Folkmanis <girtsf@users.noreply.github.com>
2024-03-10 15:24:39 -0700

    keymap 2024-03-10

--- keyboards/girtsf/walrus1/keymaps/default/keymap.c
+++ keyboards/girtsf/walrus1/keymaps/default/keymap.c
@@ -10,6 +10,7 @@ enum layer_names {
 
 // Activate fn layer when held, send ESC when tapped.
 #define _ESC_FN LT(_FN, KC_ESC)
+#define _ESC_BS LT(_FN, KC_BSPC)
 
 #define _FN MO(_LAYER_FN)
 
@@ -23,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,                   KC_F18 , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL ,
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_LBRC,                   KC_RBRC, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
         KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_F14 ,                   KC_F19 , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , _FN    , KC_F16 , KC_F17 , KC_F20 , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-        KC_LWIN, KC_LEFT, KC_RGHT, KC_LALT, KC_SPC ,          _ESC_FN, KC_BSPC, KC_DEL , KC_F21 ,          KC_ENT , KC_RALT, KC_UP  , KC_DOWN, KC_RCTL
+        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , _FN    , KC_BSPC, KC_F17 , KC_DEL , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+        KC_LWIN, KC_LEFT, KC_RGHT, KC_LALT, KC_SPC ,          _ESC_FN, KC_ENT , KC_ENT , _ESC_BS,          _______, KC_RALT, KC_UP  , KC_DOWN, KC_RCTL
     ),
     [_LAYER_FN] = LAYOUT(
         //                                                                    ||
